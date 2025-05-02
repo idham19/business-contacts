34ca98183c3e081ce9523e6cae8cb50864dd2b3a
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2025-05-02 15:10:58 -0400

    docker file added for server and clien, docker compose add to main directory and db.js added to server

---
+++ server/db.js
@@ -0,0 +1,9 @@
+// server/db.js
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: console.log
+});
+
+module.exports = sequelize;
